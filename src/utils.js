30e38b6c1f63cf4eb21690e59d06d144a4fd1cc8
Michał Gołębiowski <m.goleb@gmail.com>
2013-03-18 21:52:43 +0100

    documented u.forEach

--- src/utils.js
+++ src/utils.js
@@ -30,6 +30,14 @@ var u = {};
         };
     };
 
+    /**
+     * A faster version of <code>Array.prototype.forEach</code>. Applies <code>callback</code> to each member of
+     * <code>array</code> not strictly equal to <code>undefined</code>.
+     *
+     * @param {Array} array
+     * @param {Function} callback  A function applied on every element; takes element as the first parameter
+     *                             and its index in <code>array</code> as the second one.
+     */
     u.forEach = function forEach(array, callback) {
         var element;
         for (var i = 0, length = array.length; i < length; i++) {
