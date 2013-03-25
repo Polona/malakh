28c713e1032d14673d4560bb2be9dccc3e28c470
Michał Gołębiowski <m.goleb@gmail.com>
2013-03-25 15:12:50 +0100

    optimize Math.log2 polyfill

--- src/polyfills/static_methods.js
+++ src/polyfills/static_methods.js
@@ -1,5 +1,9 @@
-if (!Math.log2) {
-    Math.log2 = function log2(val) {
-        return Math.log(val) / Math.log(2);
-    };
-}
+(function () {
+    var log2;
+    if (!Math.log2) {
+        log2 = Math.log(2);
+        Math.log2 = function log2(val) {
+            return Math.log(val) / log2;
+        };
+    }
+})();
