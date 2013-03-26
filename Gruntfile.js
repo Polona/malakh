6a26357097600dafa33db641a6d01536f5a5f62c
Michał Gołębiowski <m.goleb@gmail.com>
2013-03-26 17:21:17 +0100

    added grunt-devtools

--- Gruntfile.js
+++ Gruntfile.js
@@ -371,6 +371,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-devtools');
 
     // Default grunt
     grunt.registerTask('default', ['build:*:*', 'jshint', 'uglify', 'dist:*']);
